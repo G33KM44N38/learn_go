27622feba2461afd09c8774e1dae9f19844aa5de
Kylian <kylianmayanga@gmail.com>
2022-09-15 16:50:56 +0200

    UPDATE: reorganize the work tree in module without changing the functionnality

--- go.dev/greetings/greetings.go
+++ go.dev/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"log"
 	"math/rand"
 	"time"
 )
@@ -19,23 +20,25 @@ func Hello(name string) (string, error) {
 }
 
 // Hellos returns a map that associates each of the named people
-// with a greeting message
-func Hellos(name []string) (map[string]string, error) {
+// with a greeting message.
+func Hellos(names []string) (map[string]string, error) {
 	// A map to associate names with messages.
 	messages := make(map[string]string)
-	for _, name := range name {
+	// Loop through the received slice of names, calling
+	// the Hello function to get a message for each name.
+	for _, name := range names {
 		message, err := Hello(name)
 		if err != nil {
 			return nil, err
 		}
-		// In the map, associate the rertrieved message with
+		// In the map, associate the retrieved message with
 		// the name.
 		messages[name] = message
 	}
 	return messages, nil
 }
 
-// init sets initial values for variables used in the function.
+// Init sets initial values for variables used in the function.
 func init() {
 	rand.Seed(time.Now().UnixNano())
 }
@@ -50,7 +53,26 @@ func randomFormat() string {
 		"Hail, %v! Well met!",
 	}
 
-	// Return a randomly selected message format by specifying
-	// a random index for the slice of formats.
+	// Return one of the message formats selected at random.
 	return formats[rand.Intn(len(formats))]
+}
+
+func Greeting() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
--- go.dev/hello/go.mod
+++ go.dev/hello/go.mod
@@ -4,4 +4,12 @@ go 1.19
 
 replace example.com/greetings => ../greetings
 
-require example.com/greetings v0.0.0-00010101000000-000000000000
+require (
+	example.com/greetings v0.0.0-00010101000000-000000000000
+	example.com/module1 v0.0.0-00010101000000-000000000000
+	example.com/module2 v0.0.0-00010101000000-000000000000
+)
+
+replace example.com/module1 => ../module1
+
+replace example.com/module2 => ../module2
